45e83057ac357d72f08863c311df1cc106302ac1
Antoni <antoni.romanski@haw-hamburg.de>
2021-09-17 10:14:10 +0200

    add draw grid with rectangles

--- sketch.js
+++ sketch.js
@@ -11,11 +11,11 @@ function grid(numberOfFields) {
   let grid = [];
   const gridSize = 800 / numberOfFields;
 
-  for (let i = 0; i < numberOfFields; i++) {
-    // const element = grid[i];
-    grid[i] = rect(0 + i * gridSize, 0, gridSize, gridSize);
+
+  // draw a rectangle numberOfFields^2 times
+  for (let index = 0; index < numberOfFields; index++) {
+    for (let i = 0; i < numberOfFields; i++) {
+      grid[i] = rect(0 + i * gridSize, index * gridSize, gridSize, gridSize);
+    }
   }
-
-
-
 }
