05b9dbcc9ddf731f6b5fcf9b583a777267d90d28
antoni <antoni.romanski@haw-hamburg.de>
2022-02-25 03:23:42 +0100

    start playerinput controller

--- sketch.js
+++ sketch.js
@@ -1,8 +1,38 @@
+let snakeLength = 0
+
+const Direction = {
+  Default: "default",
+  Up: "up",
+  Left: "left",
+  Down: "down",
+  Right: "right",
+}
+
+Direction.freeze()
+
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-	// put setup code here
+  createCanvas(windowWidth, windowHeight)
+  rect(200, 200, 200)
 }
 
 function draw() {
-	// put drawing code here
+  background(230)
+  text(key, 33, 65)
+}
+
+function playerInput(key) {
+  switch (key) {
+    case "w":
+      return "up"
+    case "a":
+      return "left"
+    case "s":
+      return "down"
+    case "d":
+      return "right"
+    case "ArrowUp":
+      return "up"
+    case "ArrowLeft":
+      return "left"
+  }
 }
