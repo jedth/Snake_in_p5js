483e36fcfb55b40c2bd4152d14a712ac30ad0b75
antoni <jedth@veril.com>
2022-03-05 20:09:35 +0100

    change to oop

--- player.js
+++ player.js
@@ -1,3 +1,33 @@
 class Player {
-  constructor() {}
+  // length array (with previous positions)
+
+  constructor(canvasX, canvasY) {
+    this.size = createVector(20, 20)
+    this.pos = createVector(canvasX / 2, canvasY / 2)
+  }
+
+  update = (key) => {
+    switch (key) {
+      case "ArrowUp":
+      case "w":
+        this.pos.y -= this.size.y
+        break
+      case "ArrowLeft":
+      case "a":
+        this.pos.x -= this.size.x
+        break
+      case "ArrowDown":
+      case "s":
+        this.pos.y += this.size.y
+        break
+      case "ArrowRight":
+      case "d":
+        this.pos.x += this.size.x
+        break
+    }
+  }
+
+  render = () => {
+    rect(this.pos.x, this.pos.y, this.size.x, this.size.y)
+  }
 }
--- sketch.js
+++ sketch.js
@@ -1,41 +1,28 @@
-// static game properties
-// const canvas = createVector(800, 800)
-
-let playerSize
-
-// dynamic game properties
-let playerPos
-
-let snakeLength = 1 // use an array instead and then just use len attribute
+let canvas
+let player
 
 function setup() {
+  canvas = createVector(800, 800)
+  player = new Player(canvas.x, canvas.y)
   createCanvas(canvas.x, canvas.y)
-  frameRate(5)
+  frameRate(10)
   fill(0)
-  playerSize = createVector(20, 20)
-  playerPos = createVector(canvas.x / 2, canvas.y / 2)
+  noStroke()
 }
 
 function draw() {
   background(200)
-  text(playerInput(key), 33, 65) // for debugging
 
-  // player input
-  if (playerInput(key) === "up" || playerInput(key) === "down")
-    playerY = position(playerY, playerInput(key))
-  else playerX = position(playerX, playerInput(key))
-  // TODO: find way to diferentiate between where the player is out of ranger to
-  // add the right amount to player position
-  if (!inRange(playerX, playerY)) {
-    if (playerX < 0) {
-      playerX += canvas.x
-    }
-  }
-  // player render
-  rect(playerX, playerY, playerSize.x, playerSize.y)
+  player.update(key)
+  player.render()
 }
 
-/// check if the player ist still on the field
+/**
+ *
+ * @param {x of canvas} x
+ * @param {y of canvas} y
+ * @returns
+ */
 function inRange(x, y) {
   switch (x) {
     case x < 0 || x + playerSize.x > canvas.x:
@@ -44,46 +31,5 @@ function inRange(x, y) {
       return false
     default:
       return true
-  }
-}
-
-/// reads player input and returns direction
-function playerInput(key) {
-  // TODO: change strings to p5js constants
-  switch (key) {
-    case "w":
-      return "up"
-    case "a":
-      return "left"
-    case "s":
-      return "down"
-    case "d":
-      return "right"
-    case "ArrowUp":
-      return "up"
-    case "ArrowLeft":
-      return "left"
-    case "ArrowDown":
-      return "down"
-    case "ArrowRight":
-      return "right"
-    default:
-      return "stop"
-  }
-}
-
-function position(pos, inp) {
-  // pos = playerXY // inp = up | left | down | right
-  switch (inp) {
-    case "up":
-      return pos - playerSize.y
-    case "left":
-      return pos - playerSize.x
-    case "down":
-      return pos + playerSize.y
-    case "right":
-      return pos + playerSize.x
-    case "stop":
-      return pos
   }
 }
