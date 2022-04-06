80adb1af3412620fc6ea461ec1dcff82ecf6d805
antoni <antoni.romanski@haw-hamburg.de>
2022-04-06 18:33:33 +0200

    add outOfBounds Functions

--- player.js
+++ player.js
@@ -1,9 +1,11 @@
 class Player {
   // length array (with previous positions)
+  size
+  pos
 
-  constructor(canvasX, canvasY) {
+  constructor() {
     this.size = createVector(20, 20)
-    this.pos = createVector(canvasX / 2, canvasY / 2)
+    this.pos = createVector(canvas.x / 2, canvas.y / 2)
   }
 
   update = (key) => {
@@ -23,6 +25,48 @@ class Player {
       case "ArrowRight":
       case "d":
         this.pos.x += this.size.x
+        break
+    }
+  }
+
+  /**
+   * checks if player is still in range
+   */
+  outOfBounds = () => {
+    switch (this.pos.x) {
+      case this.pos.x < 0 || this.pos.x + this.size.x > canvas.x:
+        return false
+      case this.pos.y < 0 || this.pos.y + this.size.y > canvas.y:
+        return false
+      default:
+        return true
+    }
+  }
+
+  outOfBoundsSetPosition = () => {
+    console.log(this.pos.x)
+    switch (this.pos) {
+      // x is to small
+      case this.pos.x < 0:
+        this.pos.x = canvas.x - this.size.x
+        console.log("setting x")
+        break
+      // x is to big
+      case this.pos.x > canvas.x - this.size.x:
+        console.log("setting x")
+        this.pos.x = 0
+        break
+      case this.pos.y < 0:
+        console.log("setting y")
+        this.pos.y = canvas.y - this.size.y
+        break
+      // x is to big
+      case this.pos.y > canvas.y - this.size.y:
+        console.log("setting y")
+        this.pos.y = 0
+        break
+
+      default:
         break
     }
   }
--- sketch.js
+++ sketch.js
@@ -1,9 +1,9 @@
-let canvas
 let player
+let canvas
 
 function setup() {
   canvas = createVector(800, 800)
-  player = new Player(canvas.x, canvas.y)
+  player = new Player()
   createCanvas(canvas.x, canvas.y)
   frameRate(10)
   fill(0)
@@ -14,22 +14,6 @@ function draw() {
   background(200)
 
   player.update(key)
+  player.outOfBoundsSetPosition()
   player.render()
-}
-
-/**
- *
- * @param {x of canvas} x
- * @param {y of canvas} y
- * @returns
- */
-function inRange(x, y) {
-  switch (x) {
-    case x < 0 || x + playerSize.x > canvas.x:
-      return false
-    case y < 0 || y + playerSize.y > canvas.y:
-      return false
-    default:
-      return true
-  }
 }
